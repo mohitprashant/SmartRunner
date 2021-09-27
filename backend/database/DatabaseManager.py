c98788230ffc521963e849066f37b98f2b1327fb
Vevek Selvam <vevekselvam@gmail.com>
2021-09-28 00:43:00 +0800

    1

--- backend/database/DatabaseManager.py
+++ backend/database/DatabaseManager.py
@@ -1,22 +1,41 @@
-# from firebase import firebase
-
-import firebase_admin as fa 
+import firebase_admin as fa
 from firebase_admin import credentials
 from firebase_admin import firestore
-from firebase_admin import storage
 
-from google.cloud import storage
+# from firebase import firebase
+# from firebase_admin import storage
+# from google.cloud import storage
 # from google.auth import load_credentials_from_file
-
-import requests
+# import requests
 
 cred = credentials.Certificate("../serviceAccountKey.json")
 fa.initialize_app(cred)
-
 db = firestore.client()
 
-subjects = db.collection(u'subjects')
-docs = subjects.stream()
 
-for doc in docs:
-    print(f'{doc.id} => {doc.to_dict()}')
+def get_subjects():
+    subjects = db.collection('subjects').get()
+    items = []
+
+    for subject in subjects:
+        items.append(subject.to_dict())
+        # print(doc.get())
+        # print(f'{doc.id} => {doc.to_dict()}')
+
+    print(items)
+    return items
+
+
+def get_topics(subject):
+    topics = db.collection('subjects').document(subject).collections()
+
+    for topic in topics:
+        print(topic)
+        for doc in topic:
+            print(doc)
+
+    print(topics)
+
+
+#get_subjects()
+get_topics('Mathematics')
