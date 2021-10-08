caf256831fc217f1b61b2796915e247fe4cef607
Vevek Selvam <vevekselvam@gmail.com>
2021-10-09 03:14:45 +0800

    Add tests for AccountManager

--- backend/tests/test_account_manager.py
+++ backend/tests/test_account_manager.py
@@ -1,6 +1,29 @@
 import unittest
 
+from backend.account import AccountManager
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)  # add assertion here
+
+class TestCreateAccount(unittest.TestCase):
+    def test_create_already_exists(self):
+        result = AccountManager.create_account('example@mail.com', '123456')
+        expected_result = None  # account already exists
+        self.assertEqual(expected_result, result)
+
+
+class TestCreateConfirmPassword(unittest.TestCase):
+    def test_create_password_issue(self):
+        self.assertRaises(ValueError, AccountManager.create_account_confirm_password, 'example@mail.com', '123456',
+                          '12345')
+
+
+class TestSignIn(unittest.TestCase):
+    def test_sign_in_exist(self):
+        result = AccountManager.sign_in('example@mail.com', '123456')
+        local_id = result['localId']
+        expected_result = 'mwoWFrnH5wa8Xcv1VlJDXaeBNP92'  # account localId
+        self.assertEqual(expected_result, local_id)
+
+    def test_sign_in_not_exist(self):
+        result = AccountManager.sign_in('non_existent@mail.com', '123456')
+        expected_result = None  # account does not exist
+        self.assertEqual(expected_result, result)
