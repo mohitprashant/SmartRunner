85aa96646a233fdf12ec7291aaa0efc0405c7547
Vevek Selvam <vevekselvam@gmail.com>
2021-10-19 19:01:36 +0800

    Refactor Leaderboard Manager

--- backend/database/FirebaseManager.py
+++ backend/database/FirebaseManager.py
@@ -1,10 +1,10 @@
 import pathlib
 import firebase_admin as fa
 import sys
-
-sys.path.insert(0, str(pathlib.Path(__file__).parent.resolve()) + "/../..")
 from firebase_admin import credentials
 from firebase_admin import firestore
+
+sys.path.insert(0, str(pathlib.Path(__file__).parent.resolve()) + "/../..")
 
 cred = credentials.Certificate(str(pathlib.Path(__file__).parent.resolve()) + "/../serviceAccountKey.json")
 
