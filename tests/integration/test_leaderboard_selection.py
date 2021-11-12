648c38119daf51fe3f6d380b7419081419acbf8c
Vevek Selvam <vevekselvam@gmail.com>
2021-11-13 03:55:15 +0800

    Add leaderboard selection test

--- tests/integration/test_leaderboard_selection.py
+++ tests/integration/test_leaderboard_selection.py
@@ -4,7 +4,7 @@ import pygame
 
 sys.path.insert(1, '../../')
 
-from frontend.pages.topic_leaderboard import TopicLeaderboardPage
+from frontend.pages.leaderboardselection import LeadSelectPage
 
 
 class TestTopicLeaderboard(unittest.TestCase):
@@ -12,27 +12,20 @@ class TestTopicLeaderboard(unittest.TestCase):
         pygame.init()
         screen = pygame.display.set_mode((720, 480), pygame.RESIZABLE)
 
-        prev_page = "main_menu"
-        subject = "test_subject"
-        topic = "test_topic"
-        topic_leaderboard = ["test"]
+        prev_page = "topic_leaderboard"
+        leaderboardlist = ["Test1", "Test2"]
         username = "integrationUser1@mail.com"
 
-        topic_leaderboard_page = TopicLeaderboardPage(screen)
+        leaderboard_selection_page = LeadSelectPage(screen)
 
-        topic_leaderboard_page.input_data["prev_page"] = prev_page
-        topic_leaderboard_page.input_data["subject"] = subject
-        topic_leaderboard_page.input_data["topic"] = topic
-        topic_leaderboard_page.input_data["topic_leaderboard"] = topic_leaderboard
-        topic_leaderboard_page.input_data["username"] = username
+        leaderboard_selection_page.input_data["prev_page"] = prev_page
+        leaderboard_selection_page.input_data["leaderboardlist"] = leaderboardlist
+        leaderboard_selection_page.input_data["username"] = username
 
-        topic_leaderboard_page.set_components(screen)
+        leaderboard_selection_page.set_components(screen)
 
-        triggered_component_list = [topic_leaderboard_page.components["share_button"], topic_leaderboard_page.components["return_button"]]
-        topic_leaderboard_page.page_function(triggered_component_list)
+        triggered_component_list = [leaderboard_selection_page.components["exit_button"], leaderboard_selection_page.components["textbox_button_list"]]
+        leaderboard_selection_page.page_function(triggered_component_list)
 
-        self.assertTrue(topic_leaderboard_page.output_data["prev_page"], prev_page)
-        self.assertTrue(topic_leaderboard_page.output_data["subject"], subject)
-        self.assertTrue(topic_leaderboard_page.output_data["topic"], topic)
-        self.assertTrue(topic_leaderboard_page.output_data["topic_leaderboard"], topic_leaderboard)
-        self.assertTrue(topic_leaderboard_page.output_data["username"], username)
+        self.assertTrue(leaderboard_selection_page.output_data["current_page"], "topic_leaderboard")
+        self.assertTrue(leaderboard_selection_page.output_data["prev_page"], prev_page)
