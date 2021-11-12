aab7a44554eabec4397d5ed79a14025e4ec0dbaa
Guangyan Tang <guangyan_tang@hotmail.com>
2021-11-12 13:00:11 +0800

    Add get_avatar and user_exists function

--- backend/account/AccountHelper.py
+++ backend/account/AccountHelper.py
@@ -36,10 +36,59 @@ def add_to_user_collection(username):
 
 
 def set_avatar(username, avatar):
-    existing_user = db.collection("users").where("username", "==", username).get()
+    """
+    :param username: Username of the user
+    :param avatar: Avatar value to be set
+    :return: Returns false if user does not exist. True if successfully set
+    """
+    if type(username) is not str or type(avatar) is not str:
+        raise Exception("Given arguments are not of type str")
 
-    if len(existing_user) == 0:
-        raise Exception("User dooes not exist")
+    if username == "" or avatar == "":
+        raise Exception("Given arguments cannot be empty")
 
-    existing_user = existing_user[0]
+    if not user_exists(username):
+        return False
+
+    existing_user = db.collection("users").where("username", "==", username).get()[0]
     db.collection("users").document(existing_user.id).update({"avatar": avatar})
+    return True
+
+
+def get_avatar(username):
+    """
+    :param username: Username of the user
+    :return: returns None if the user does not exist, else returns the string value of the avatar
+    """
+    if type(username) is not str:
+        raise Exception("Given arguments are not of type str")
+
+    if username == "":
+        raise Exception("Given arguments cannot be empty")
+
+    if not user_exists(username):
+        return None
+
+    avatar = db.collection("users").where("username", "==", username).get()[0].to_dict()["avatar"]
+    return avatar
+
+
+def user_exists(username):
+    """
+    :param username: Username of the user
+    :return: True if user exists
+    :raises: An exception if database is corrupted
+    """
+    if type(username) is not str:
+        raise Exception("Given arguments are not of type str")
+
+    if username == "":
+        return False
+
+    existing_user = db.collection("users").where("username", "==", username).get()
+    if len(existing_user) == 0:
+        return False
+    elif len(existing_user) == 1:
+        return True
+    else:
+        raise Exception("Duplicate user entries in database. Please remove the duplicate entry")
