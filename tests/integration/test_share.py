d44d4ba765c64e31ddb2e48282d2fffe221d6701
Vevek Selvam <vevekselvam@gmail.com>
2021-11-13 03:55:23 +0800

    Add test for share

--- tests/integration/test_share.py
+++ tests/integration/test_share.py
@@ -4,35 +4,32 @@ import pygame
 
 sys.path.insert(1, '../../')
 
-from frontend.pages.topic_leaderboard import TopicLeaderboardPage
+from frontend.pages.share import SharePage
 
 
-class TestTopicLeaderboard(unittest.TestCase):
-    def test_room_creation(self):
+class TestShare(unittest.TestCase):
+    def test_share(self):
         pygame.init()
         screen = pygame.display.set_mode((720, 480), pygame.RESIZABLE)
 
         prev_page = "main_menu"
-        subject = "test_subject"
-        topic = "test_topic"
-        topic_leaderboard = ["test"]
+        roomID = "id123"
+        room_password = "123"
         username = "integrationUser1@mail.com"
 
-        topic_leaderboard_page = TopicLeaderboardPage(screen)
+        share_page = SharePage(screen)
 
-        topic_leaderboard_page.input_data["prev_page"] = prev_page
-        topic_leaderboard_page.input_data["subject"] = subject
-        topic_leaderboard_page.input_data["topic"] = topic
-        topic_leaderboard_page.input_data["topic_leaderboard"] = topic_leaderboard
-        topic_leaderboard_page.input_data["username"] = username
+        share_page.input_data["prev_page"] = prev_page
+        share_page.input_data["roomID"] = roomID
+        share_page.input_data["room_password"] = room_password
+        share_page.input_data["username"] = username
 
-        topic_leaderboard_page.set_components(screen)
+        share_page.set_components(screen)
 
-        triggered_component_list = [topic_leaderboard_page.components["share_button"], topic_leaderboard_page.components["return_button"]]
-        topic_leaderboard_page.page_function(triggered_component_list)
+        triggered_component_list = [share_page.components["twitter_button"], share_page.components["facebook_button"], share_page.components["return_button"]]
+        share_page.page_function(triggered_component_list)
 
-        self.assertTrue(topic_leaderboard_page.output_data["prev_page"], prev_page)
-        self.assertTrue(topic_leaderboard_page.output_data["subject"], subject)
-        self.assertTrue(topic_leaderboard_page.output_data["topic"], topic)
-        self.assertTrue(topic_leaderboard_page.output_data["topic_leaderboard"], topic_leaderboard)
-        self.assertTrue(topic_leaderboard_page.output_data["username"], username)
+        self.assertTrue(share_page.output_data["prev_page"], prev_page)
+        self.assertTrue(share_page.output_data["room_ID"], roomID)
+        self.assertTrue(share_page.output_data["room_password"], room_password)
+        self.assertTrue(share_page.output_data["username"], username)
