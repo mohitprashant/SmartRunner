2a2a315dcf30670e59a7a313398678a56205edc6
Mohit Prashant <32471894+mohitprashant@users.noreply.github.com>
2021-11-14 10:22:43 +0800

    force_accept_connection ----> do not run unnecessarily

--- frontend/pages/game_play.py
+++ frontend/pages/game_play.py
@@ -56,8 +56,20 @@ class Game(Page):
         self.is_client = False
         self.multiplayer = multiplayer
 
-
-
+    
+    def force_accept_connection(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((HOST, PORT))
+            s.listen()
+            conn, addr = s.accept()
+            with conn:
+                print('Connected by', addr)
+                while True:
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+                    conn.sendall(data)
+                
 
     def set_components(self, screen):
         # background
@@ -251,7 +263,7 @@ class Game(Page):
         
         # player sprite
         game_image_rel_x = 4 / 10
-        game_image_rel_y = 0.55
+        game_image_rel_y = 0.47
         game_image_rel_width = 1 / 7
         game_image_rel_height = 0.3
         player = pygame.image.load('assets/Sprites/'+self.avatar+'2.png')
@@ -305,7 +317,7 @@ class Game(Page):
             self.avatarstate = (self.avatarstate + 1) %6
             
             game_image_rel_x = 4 / 10
-            game_image_rel_y = 0.54
+            game_image_rel_y = 0.5
             game_image_rel_width = 1 / 7
             game_image_rel_height = 0.3
             player = pygame.image.load('assets/Sprites/'+self.avatar+str(self.avatarstate)+'.png')
@@ -419,7 +431,7 @@ class Game(Page):
         game_image_rel_y = 0.02
         game_image_rel_width = 1 / 10
         game_image_rel_height = 1 / 10
-        exit_btn = pygame.image.load('assets/Buttons/btn_end.png')
+        exit_btn = pygame.image.load('assets/Buttons/btn_back.png')
         exit_btn = ImageDisplay("exit_btn", screen, game_image_rel_x, game_image_rel_y,
                                         game_image_rel_width, game_image_rel_height,exit_btn)
         self.components["exit_btn"] = exit_btn
